f8200f1dea413a7e3340d56c3dd898dbf531d1c5
VasantLong <157333230+VasantLong@users.noreply.github.com>
2024-12-21 12:11:48 +0800

    Milestone 1 Layout Optimization

--- sweeperScript.js
+++ sweeperScript.js
@@ -1,20 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
   /* 网格初步建立 */
   const board = document.getElementById('board');
+  const grid = document.getElementById('grid');
   function createBoard(rows, cols) {
     for (let r = 0; r < rows; r++) {
+      const tr = document.createElement('tr');
+      grid.appendChild(tr);
       for (let c = 0; c < cols; c++) {
-        const cell = document.createElement('div');
+        const cell = document.createElement('td');
         cell.className = 'cell';
         cell.dataset.row = r;
         cell.dataset.col = c;
-        board.appendChild(cell);
+        tr.appendChild(cell);
       }
     }
   }
-  createBoard(9,9);
+  createBoard(9, 9);
   console.log("createboard");
 })
+
+
+
+
 /*function main() {
   console.log("main")
   createBoard(9, 9)
